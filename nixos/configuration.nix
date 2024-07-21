c801c3a2f23c00925fc4ff23e801667514723874
AlpaZZ <alfarizki181@gmail.com>
2024-07-21 08:00:14 +0700

    a

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,16 +10,7 @@
       ./time-zone
       ./fonts
       ./nixvim
-     # ./clean
     ];
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the XFCE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.desktopManager.cinnamon.enable = true;
-
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Allow unfree packages
