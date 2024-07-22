2dc4a81324031ba18380edd34f23bd08906db626
Alfarizki <155267889+AlpaZZ@users.noreply.github.com>
2024-07-22 07:42:42 +0700

    Update gnome

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,7 +18,7 @@
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   #services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.desktopManager.cinnamon.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
