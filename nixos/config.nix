82270c64f63edc1d9274aea0e49e2559659da50c
AlpaZZ <alfarizki1810@gmail.com>
2024-07-29 19:45:44 +0700

    nixpkgs

--- nixos/config.nix
+++ nixos/config.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   options,
+  self,
   ...
 }:
 
@@ -19,6 +20,7 @@
     ./security
     ./system
     ./nixpkgs
+
   ];
 
   hardware.sane = {
@@ -27,23 +29,5 @@
     disabledDefaultBackends = [ "escl" ];
   };
 
-
-  # Optimization settings and garbage collection automation
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
 
 }
--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -1,1 +1,18 @@
-{ system.stateVersion = "24.05"; }
+{ self, pkgs, lib, inputs, ...}: 
+{
+  # imports = [ inputs.nix-gaming.nixosModules.default ];
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+  system.stateVersion = "24.05";
+}
